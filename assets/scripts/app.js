5f1d2cd2b4c46d4dc1eaac2a80084c5f96bc7bb4
kayatk <thillaikayatk@gmail.com>
2024-12-30 23:50:27 +0100

    Functions for calc buttons

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,33 @@
 const defaultResult =0;
 let currentResult = defaultResult;
-currentResult =(currentResult+10) *3/2 -1;
-let desciption =`(${defaultResult}+10)*3/2 -1`
-outputResult(currentResult,desciption);
+// currentResult =(currentResult+10) *3/2 -1;
+// let desciption =`(${defaultResult}+10)*3/2 -1`
+// outputResult(currentResult,desciption);
+function getnumberInput(){
+    return parseInt(userInput.value)
+}
+
+function add(){
+    const enteredNo=getnumberInput()
+    const description=`${currentResult}+${enteredNo}`
+    currentResult = currentResult + enteredNo;
+    outputResult =(currentResult,description);
+}
+function substract(){
+    const enteredNo=getnumberInput()
+    const description=`${currentResult}-${enteredNo}`
+    currentResult = currentResult - userInput.value;
+    outputResult =(currentResult,description);
+}
+function divide(){
+    const enteredNo=getnumberInput()
+    const description=`${currentResult}/${enteredNo}`
+    currentResult = currentResult / userInput.value;
+    outputResult =(currentResult,description);
+}
+function multiply(){
+    const enteredNo=getnumberInput()
+    const description=`${currentResult}*${enteredNo}`
+    currentResult = currentResult * userInput.value;
+    outputResult =(currentResult,description);
+}
